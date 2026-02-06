ab8354b2cd8749ac5e41274894bde397ec209a09
Bedri YAGIZ <bedri.yagiz@bsc.es>
2026-02-06 12:19:50 +0100

    Main_Ev : load .pkl is changed

--- Main_Ev.py
+++ Main_Ev.py
@@ -41,15 +41,16 @@ if __name__ == "__main__":
     MODEL_BASENAME = (
         f"airfoil_Re{int(config.RE/1e6)}M_AoA{int(config.AOA):02d}_{config.OBJECTIVE.upper()}"
     )
-    #ms = MODEL_BASENAME + "_001"
     # directory where the Python script lives
-    SCRIPT_DIR = Path(__file__).resolve().parent
-    ms = SCRIPT_DIR/"runs" /"optuna_ppo"/"airfoil_Re3M_AoA00_CL"/"trial_0031"/"model"
-    
+    # SCRIPT_DIR = Path(__file__).resolve().parent
+    # ms = SCRIPT_DIR/"runs" /"optuna_ppo"/"airfoil_Re3M_AoA00_CL"/"trial_0031"/"model"
+     
+    ms = MODEL_BASENAME + "_001"
     # --- rebuild env and load normalization stats ---
     NUM_ENVS = 1
     vec_env = DummyVecEnv([create_env(i) for i in range(NUM_ENVS)])
-    vec_env = VecNormalize.load(str(ms.with_suffix(".pkl")), vec_env)
+    #vec_env = VecNormalize.load(str(ms.with_suffix(".pkl")), vec_env)
+    vec_env = VecNormalize.load(ms + ".pkl", vec_env)
     vec_env.training = False       # evaluation mode
     vec_env.norm_reward = False    # don’t normalize rewards during eval
 
